b57848d5edade4261809b9c2ddc0e5f7ddd9b05d
kusi <lawcubegsm@gmail.com>
2023-05-18 12:41:46 +0000

    Update page layout settings

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,8 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Header from '@/components/header/header'
+import Nav from '@/components/nav/nav'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,9 +14,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-      {children}
+      <body className={`${inter.className} dark  `}>
+        <div className="flex gap-4 bg-gray_bg dark:bg-white py-3 px-3 text-black min-h-screen ">
+           <div className='w-60 '>
+             <Nav />
+           </div>
+           <div className='w-full '>
+             <div > 
+             <Header />
+             </div>
+             {children} 
+           </div>
+        </div>
       </body>
     </html>
   )
 }
+// 
