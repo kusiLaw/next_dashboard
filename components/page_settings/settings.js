1574b47ad3ba30739a4e8b07186acebfa478925d
kusi <lawcubegsm@gmail.com>
2023-05-26 20:43:43 +0000

    Improve select input code

--- components/page_settings/settings.js
+++ components/page_settings/settings.js
@@ -7,8 +7,13 @@ import { useTheme } from 'next-themes'
 
 
 const Settings = ({setOpenSettings}) => {
- const { theme, setTheme } = useTheme()
+  const { theme, setTheme } = useTheme()
+  const defaultInput = {
+   'light': 'Default',
+   'dark': 'DarkLight'
+  }
 
+  
   return (
     <div className='absolute bg-white rounded-md top-3 shadow-2xl right-0 bottom-0 p-6  z-50'>
       <div className='flex items-center justify-between pb-5 border-b border-gray_bg'>
@@ -18,7 +23,13 @@ const Settings = ({setOpenSettings}) => {
         </div>
       </div>
       <div className='mt-8'>
-           <SelectInput text='Theme ' list={[['Default', 'light'], ['DarkLight', 'dark']]} setTheme = {setTheme} />
+           <SelectInput 
+           text='Theme ' 
+           name = 'theme'
+           options={[['Default', 'light'], ['DarkLight', 'dark']]} 
+            setTheme = {setTheme} 
+            defaultInput = {defaultInput[theme]}
+           />
       </div>
     </div>
   )
--- components/selectInput/selectInput.js
+++ components/selectInput/selectInput.js
@@ -1,20 +1,20 @@
 import { useRef } from "react"
 
 const SelectInput = (props) => {
-  const {text, name, setTheme, list = []} = props
-   // let refValue = useRef()
-   // refValue.current = e.target[e.target.selectedIndex].text; 
-   // console.log(refValue)
-   // console.log(refValue.current, 'redenring')
+  const {text, name, setTheme, defaultInput = '', options = []} = props
+
+   console.log(defaultInput)
    return (
    <label className='font-semibold'>
     {text} : 
    <select  name={name} className='ml-5 p-2 border-0 font-normal active:border-0 bg-white dark:bg-white rounded-sm' 
    onChange={e => {setTheme(e.target.value)}}  
-   // defaultValue = { refValue.current}
+   // defaultValue = {defaultInput}
+   defaultValue="DarkLight"
    >
-     {list.map(el =>(
-      <option className='hover:bg-blue_contest dark:hover:bg-blue_contest' value={Array.isArray(el) ? el[1] : el }>{Array.isArray(el) ? el[0] : el }</option>
+     {options.map(el =>(
+      <option className='hover:bg-blue_contest dark:hover:bg-blue_contest' 
+      value={Array.isArray(el) ? el[1] : el }>{Array.isArray(el) ? el[0] : el }</option>
      ))}
    </select>
  </label>
