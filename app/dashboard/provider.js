f330aeed748dcb9c151175e07d43302f12e6d44c
kusi <lawcubegsm@gmail.com>
2023-05-30 23:24:26 +0000

    Add toggle button controller context

--- app/dashboard/provider.js
+++ app/dashboard/provider.js
@@ -4,17 +4,19 @@ import { createContext, useState } from 'react'
 // All shared data use by dashboard only should be here ..
 
 export const navToggleContext = createContext()
-// export const navCollapseContext = createContext()
+export const toggleBtnContext = createContext()
 
 
 export function DashboardProvider({ children }) {
 
  const [navMount, setNavMount]  = useState(true) 
-
+ const [displayToggleBtn, setDisplayToggleBtn] = useState(false)
 
  return (
  <navToggleContext.Provider value={{navMount, setNavMount}}>
-    {children}
+    <toggleBtnContext.Provider value={{displayToggleBtn, setDisplayToggleBtn}}>
+       {children}
+    </toggleBtnContext.Provider>
  </navToggleContext.Provider>
  )
 }
