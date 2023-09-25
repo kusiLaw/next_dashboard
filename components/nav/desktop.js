b941378d83c9f8f7031bae5ffdcb4d296e42d0df
lawrence <lawcubegsm@gmail.com>
2023-09-25 12:09:36 +0100

    Fix nav control bug

--- components/nav/desktop.js
+++ components/nav/desktop.js
@@ -13,8 +13,8 @@ const DesktopNav = ({navCollapse, setNavCollapse}) => {
 
      <nav className={`flex flex-col ${navCollapse ? `w-[4rem]`:`w-[12rem]`} dark:text-white   bg-white dark:bg-dark_color p-4 rounded-xl drop-shadow-xl  
        h-full animate-fade-right animate-delay-500  ease-in-out duration-500`} 
-       onMouseLeave={() => { mouseControlNav & setNavCollapse(true) }}    
-       onMouseEnter={() => { mouseControlNav & setNavCollapse(false)  }}        >
+       onMouseLeave={() => {if (mouseControlNav)  setNavCollapse(true) }}    
+       onMouseEnter={() => { if (mouseControlNav) setNavCollapse(false)  }}        >
       <div className='flex flex-col gap-3  pb-4 mb-6 border-1 border-b transition duration-1000 mt-2'>
               <h1 className='dark:text-white  font-extralight'><span className='text-blue_contest italic 
               font-bold' >L</span>aky</h1>
