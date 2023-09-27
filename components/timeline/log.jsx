b7d77b91b410085afccbc8ba2740f228dd8c686d
lawrence <lawcubegsm@gmail.com>
2023-09-27 09:39:45 +0100

    Fix build error

--- components/timeline/log.jsx
+++ components/timeline/log.jsx
@@ -6,8 +6,8 @@ const LogTimeLine = ({data}) =>{
         
 <ol className="relative border-s border-gray-500 dark:border-gray-700">                  
 
-    { data.map( data =>(
-        <li className="mb-10 ms-6 border-4">
+    { data.map( (data, index) =>(
+        <li key={data.user + index} className="mb-10 ms-6 border-4">
             <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white
             dark:ring-gray-900 dark:bg-blue-900 ">
                 <img className="rounded-full shadow-lg" src={data.image} alt={data.user}/>
