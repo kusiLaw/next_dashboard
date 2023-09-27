98f3861fe6aa3bb1d015d4bc1473c6fc743f5744
lawrence <lawcubegsm@gmail.com>
2023-09-27 09:31:59 +0100

    Add advance settings

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -3,7 +3,6 @@ import { BiGridAlt, BiUser, BiPieChartAlt} from 'react-icons/bi';
 import { FiSettings} from 'react-icons/fi';
 import { FaGlobeAfrica} from 'react-icons/fa';
 
-
 const Nav = ({navCollapse = false}) => {
   return (
     <>
@@ -17,12 +16,13 @@ const Nav = ({navCollapse = false}) => {
            <LinkButton   link_path='/dashboard/widgets' id='report'> 
              <BiPieChartAlt/> <span className={`${navCollapse ? `hidden` : `inline` }`}>Widgets</span>   
            </LinkButton>
+           <LinkButton   className={"hidden md:block"} link_path='/dashboard/settings'  id='settings'> 
+                <FiSettings/> <span className={`${navCollapse ? `hidden` : `inline` }`}> Settings</span> 
+           </LinkButton>
        
         </div>
         <div className=''>
-           <LinkButton   className={"hidden md:block"} link_path='dashboard/#' > 
-                <FiSettings/> <span className={`${navCollapse ? `hidden` : `inline` }`}> Settings</span> 
-           </LinkButton>
+       
            <LinkButton t link_path= '/' > 
                 <FaGlobeAfrica/> <span className={`${navCollapse ? `hidden` : `inline` }`}> View Page</span> 
            </LinkButton>
